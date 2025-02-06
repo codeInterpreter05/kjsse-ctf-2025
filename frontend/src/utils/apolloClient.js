3e9ece0132e8475108c267978fc4a826314487f4
codeInterpreter05 <sarthupadhyay0507@gmail.com>
2025-02-06 23:47:09 +0530

    dev changes

--- frontend/src/utils/apolloClient.js
+++ frontend/src/utils/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://ctf-questions.onrender.com/',
+  uri: 'https://kjsse-ctf-2025.onrender.com/',
   cache: new InMemoryCache(),
 });
 
